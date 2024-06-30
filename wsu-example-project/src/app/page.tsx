f257d43363812e64a0dd62d20dc0d4d59277b416
Ezequiel Torres <torreszeaks@gmail.com>
2024-06-30 14:18:35 -0700

    Added lecture 2.

--- wsu-example-project/src/app/page.tsx
+++ wsu-example-project/src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
+
+function MyButton() {
+  return (
+    <button>{"I'm a button"}</button>
+  );
+}
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-    Test
-    </main>
-  );
+  <>
+    <MyButton/>
+  </>
+ );
 }
