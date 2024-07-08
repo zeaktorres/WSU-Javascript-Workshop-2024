5e1737565804d1c16b855610e718d05d928f4380
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-08 11:02:53 -0700

    Adding Chapter 4 and 5

--- wsu-example-project/src/app/page.tsx
+++ wsu-example-project/src/app/page.tsx
@@ -1,17 +1,38 @@
+"use client"
 import styles from "./page.module.css";
+import {useEffect, useState} from "react"
 
-function sumNumbers(a: number[]) {
-    let sum: number = 0
-    for (let item of a){
-        sum += item
-    }
-    return sum
-}
+
+function submit(e: any, setForm: Function) { 
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson)
+    console.log(setForm(parseInt(formJson.miles.toString()) / parseInt(formJson.gallons.toString())))
+} 
 
 export default function Home(){
+
+    let [form, setForm] = useState(null)
+
     return (
         <div className={styles.main}>
-            {[5, 10, 15].reduce((sum, element) => {return sum + element}, 0)}
+            <form onSubmit={(e) => {submit(e, setForm)}}>
+                <label>
+                    Miles Driven: <input name="miles" defaultValue="0" />
+                </label>            
+                <label>
+                    Gallons Used: <input name="gallons" defaultValue="0" />
+                </label>                        
+                <button type="submit">Submit</button>
+                {form}
+            </form>
         </div>
     );
+
+
 }
