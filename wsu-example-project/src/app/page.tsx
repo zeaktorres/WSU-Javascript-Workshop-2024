a501f01af9aff786a6c4c8a2eda05a8ce376b15b
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-09 13:34:41 -0700

    Centering buttons.

--- wsu-example-project/src/app/page.tsx
+++ wsu-example-project/src/app/page.tsx
@@ -1,38 +1,16 @@
 "use client"
 import styles from "./page.module.css";
-import {useEffect, useState} from "react"
+import Button from '@mui/material/Button';
 
-
-function submit(e: any, setForm: Function) { 
-    // Prevent the browser from reloading the page
-    e.preventDefault();
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
-
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson)
-    console.log(setForm(parseInt(formJson.miles.toString()) / parseInt(formJson.gallons.toString())))
-} 
 
 export default function Home(){
-
-    let [form, setForm] = useState(null)
-
     return (
         <div className={styles.main}>
-            <form onSubmit={(e) => {submit(e, setForm)}}>
-                <label>
-                    Miles Driven: <input name="miles" defaultValue="0" />
-                </label>            
-                <label>
-                    Gallons Used: <input name="gallons" defaultValue="0" />
-                </label>                        
-                <button type="submit">Submit</button>
-                {form}
-            </form>
+            <div>
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </div>
         </div>
     );
-
-
 }
