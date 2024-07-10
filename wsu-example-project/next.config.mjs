bd0119f838787c0b9e7c7ae7e25709b780761adb
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-10 08:42:04 -0700

    Fixing next.config.mjs

--- wsu-example-project/next.config.mjs
+++ wsu-example-project/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         unoptimized: true,
     },
     assetPrefix: process.env.ASSET_PREFIX,
-    basePath: "/wsu-example-project/public" + process.env.BASE_PATH,
+    basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
