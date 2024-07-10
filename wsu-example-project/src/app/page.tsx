c41cf26030fbe20e154ddbf6857a6ab9fd1704cf
Ezequiel Torres <torreszeaks@gmail.com>
2024-07-09 20:23:07 -0700

    Added Chapter6.

--- wsu-example-project/src/app/page.tsx
+++ wsu-example-project/src/app/page.tsx
@@ -1,16 +1,22 @@
 "use client"
 import styles from "./page.module.css";
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 
 export default function Home(){
     return (
         <div className={styles.main}>
-            <div>
-                <Button variant="text">Text</Button>
-                <Button variant="contained">Contained</Button>
-                <Button variant="outlined">Outlined</Button>
-            </div>
+            <AppBar position="static">        
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        {"Zeak's Portfolio"}
+                    </Typography>
+                    <Button color="inherit">Post</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
